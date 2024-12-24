b6eed034ab00a9b0870a7c896ac1de3e15ae1fd5
Md. Main <mainbsl4@gmail.com>
2024-12-24 12:13:36 +0600

    redis

--- project/settings.py
+++ project/settings.py
@@ -203,7 +203,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('redis://red-ctkjm2qj1k6c73cpaqtg', 6379)],
         },
     },
 }
