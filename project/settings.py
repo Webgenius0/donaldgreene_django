770f94758e887c8e34f4fa235cb10c2c0cc0889b
Md. Main <mainbsl4@gmail.com>
2024-12-24 11:33:13 +0600

    pull from hb

--- project/settings.py
+++ project/settings.py
@@ -198,7 +198,6 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
 DOMAIN_URL = os.getenv('DOMAIN_URL')
 
-<<<<<<< HEAD
 # REDIS Configuration settings
 CHANNEL_LAYERS = {
     'default': {
@@ -214,8 +213,3 @@ CHANNEL_LAYERS = {
 #         'BACKEND': 'channels.layers.InMemoryChannelLayer',
 #     },
 # }
-=======
-
-
-
->>>>>>> ee350bebc5dbe70d661ddecfb1c59266c60ee957
